a34824924ecf5110e3fb226611b7992889c8e515
Iryna Lazareva <il337733@gmail.com>
2022-08-19 17:05:36 +0300

    add simpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const cardCreated = createGalleryCard(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", cardCreated);
+
+function createGalleryCard(cards) {
+  return cards
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="
+      ${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+console.log(galleryItems);
+
+galleryEl.addEventListener("click", onCardClick);
+
+function onCardClick(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const originalImg = event.target.dataset.source;
+
+    //add from basicLightbox
+    const modalWindow = new SimpleLightbox(`
+    
+`);
+    modalWindow.show();
+}
