01c6e30c329c0c0bceee04c83b86956f8cc8ba47
Iryna Lazareva <il337733@gmail.com>
2022-08-20 22:19:55 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,30 @@
-
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 console.log(player);
-player.on('timeupdate', function (data) {
-  const currentTimeVideo = data.seconds;
-    console.log(currentTimeVideo);
-});
 const STORAGE_KEY = 'videoplayer-current-time';
-const currentTime = localStorage.getItem('videoplayer-current-time');
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTimeVideo = data.seconds;
+    console.log(currentTimeVideo);
+    localStorage.setItem(STORAGE_KEY, currentTimeVideo);
+  }, 1000)
+);
+const savedTime = localStorage.getItem(STORAGE_KEY);
+console.log(savedTime);
 
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
 
-// player
-//   .getCurrentTime()
-//     .then(function (seconds) {
-//         console.log(seconds)
-//     });
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const throttle = require('lodash.throttle');
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name="email"]'),
+  textarea: document.querySelector('[name="message"]'),
+};
+// console.log(refs)
+fullData();
+refs.form.addEventListener('input', onFormEmail)
+let formData = {}
+function onFormEmail(evt) {
+    // console.log(evt.target.name)
+    formData[evt.target.name] = evt.target.value
+    // console.log(formData)
+    const stringData = JSON.stringify(formData)
+    localStorage.setItem(STORAGE_KEY, stringData)
+}
+function fullData() {
+    const fullDataForm = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    
+    if (fullDataForm === null) {
+        return
+    }
+    refs.email.value = fullDataForm['email'] || null;
+    refs.textarea.value = fullDataForm['message'] || null;
+}
 
+refs.form.addEventListener('submit', onSubmitForm)
+
+function onSubmitForm(evt) {
+    console.log(evt)
+    evt.preventDefault();
+    
+}
